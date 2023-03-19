5e4bdae561900f6e95b2f96d6f3884777a33bb6f
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-03-20 01:37:41 +0900

    Redux Devtoolsの設定を追加

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 console.dir(store);
 
 root.render(
